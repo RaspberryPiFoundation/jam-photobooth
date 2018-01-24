0432f891687151520843d22b1e2280efe9096e90
Ben Nuttall <ben@bennuttall.com>
2018-01-24 21:27:57 +0000

    Add text annotation countdown

--- photobooth.py
+++ photobooth.py
@@ -7,7 +7,12 @@ camera = JamPiCamera()
 camera.resolution = (1024, 768)
 camera.hflip = True
 camera.start_preview()
+camera.annotate_text_size = 70
 
 while True:
     input("Press Enter ")
+    for i in reversed(range(3)):
+        camera.annotate_text = '{}...'.format(i + 1)
+        sleep(1)
+    camera.annotate_text = None
     camera.capture()
