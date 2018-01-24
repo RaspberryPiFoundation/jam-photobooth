17e08d8cb1977b1040d147275755bd781cd7887e
Ben Nuttall <ben@bennuttall.com>
2018-01-24 22:00:16 +0000

    Add exit when button held

--- photobooth.py
+++ photobooth.py
@@ -4,15 +4,27 @@ from time import sleep
 
 camera = JamPiCamera()
 
+button = Button(25, hold_time=5)
+
 camera.resolution = (1024, 768)
 camera.hflip = True
 camera.start_preview()
 camera.annotate_text_size = 70
 
-while True:
-    input("Press Enter ")
-    for i in reversed(range(3)):
-        camera.annotate_text = '{}...'.format(i + 1)
-        sleep(1)
-    camera.annotate_text = None
-    camera.capture()
+def quit():
+    global running
+    running = False
+    camera.annotate_text = "Button held. Release to exit..."
+
+button.when_held = quit
+
+running = True
+while running:
+    button.wait_for_press()
+    button.wait_for_release()
+    if running:
+        for i in reversed(range(3)):
+            camera.annotate_text = '{}...'.format(i + 1)
+            sleep(1)
+        camera.annotate_text = None
+        camera.capture()
