ba9fa6ffda9a2254f6990e1f37bd7a36056b5218
Ben Nuttall <ben@bennuttall.com>
2018-01-27 19:10:40 +0000

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo raspi-config nonint do_camera 1
+sudo apt update
+sudo apt install python3-gpiozero python3-picamera -y
+sudo pip3 install twython
+cd ~
+git clone https://github.com/raspberrypifoundation/jam-photobooth
+crontab -l | { cat; echo "@reboot python3 /home/pi/jam-photobooth/photobooth.py &"; } | crontab -
+sudo reboot
