0c78d9c9750661d9705fb17e31a38cc3ee9341e2
Jon Witts <jonwitts@users.noreply.github.com>
2018-02-24 14:10:05 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo raspi-config nonint do_camera 0
 sudo apt update
-sudo apt install python3-gpiozero python3-picamera python3-pip git -y
+sudo apt install python3-gpiozero python3-picamera python3-pip python3-pil git -y
 sudo pip3 install twython
 cd ~
 git clone https://github.com/raspberrypifoundation/jam-photobooth
