bdc4f940ba6c5f4fc513038e747d62f98dd5c4b7
Jon Witts <jonwitts@users.noreply.github.com>
2018-02-24 14:31:34 +0000

    Update setup.sh
    I had no ~/Pictures dir in Raspbian Lite...

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@ sudo apt update
 sudo apt install python3-gpiozero python3-picamera python3-pip python3-pil git -y
 sudo pip3 install twython
 cd ~
+mkdir Pictures
 git clone https://github.com/raspberrypifoundation/jam-photobooth
 crontab -l | { cat; echo "@reboot python3 /home/pi/jam-photobooth/photobooth.py &"; } | crontab -
 sudo reboot
