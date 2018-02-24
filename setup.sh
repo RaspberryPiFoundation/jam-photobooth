b396c826d754c5d314d00503d60f40d0f479aa51
Ben Nuttall <ben@bennuttall.com>
2018-02-24 14:04:35 +0000

    Add --upgrade to pip install, close #7

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 sudo raspi-config nonint do_camera 0
 sudo apt update
 sudo apt install python3-gpiozero python3-picamera python3-pip git -y
-sudo pip3 install twython
+sudo pip3 install twython --upgrade
 cd ~
 git clone https://github.com/raspberrypifoundation/jam-photobooth
 crontab -l | { cat; echo "@reboot python3 /home/pi/jam-photobooth/photobooth.py &"; } | crontab -
